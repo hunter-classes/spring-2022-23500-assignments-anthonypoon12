0d22178fe05cd52e4521153a52aab47698376b7d
anthonypoon12 <anthony@poons.us>
2022-02-01 16:22:17 -0500

    made stepPerms faster

--- assignments/recursion/staircase.cpp
+++ assignments/recursion/staircase.cpp
@@ -7,20 +7,9 @@ returns: int number of ways you can clumb the staircase in 1, 2, or 3 steps at a
 */
 
 int stepPerms(int n) {
-    int output = 0;
-    if (n==1)
-        return 1;
-    if (n-3>1)
-        output+= stepPerms(n-3);
-    else if (n-3 >= 0 && n-3 <=1)
-        output++;
-    if (n-2>1)
-        output+= stepPerms(n-2);
-    else if (n-2 >= 0 && n-2 <=1)
-        output++;
-    if (n-1>1)
-        output+= stepPerms(n-1);
-    else if (n-1 >= 0 && n-1 <=1)
-        output++;
-    return output % (int(pow(10,10) + 7));
+    if (n<3)
+        return n;
+    if (n==3)
+        return 4;
+    return stepPerms(n-1) + stepPerms(n-2) + stepPerms(n-3);
 }
