27a8571693998a08430d4871aced49d4ef7a8c02
anthonypoon12 <anthony@poons.us>
2022-02-23 16:01:41 -0500

    it doesnt crask

--- assignments/recursive-search/funcs.cpp
+++ assignments/recursive-search/funcs.cpp
@@ -1,85 +1,99 @@
 #include <iostream>
 #include <fstream>
 #include <unistd.h>
+#include <iomanip>
 
-char knight = 'N';
-char empty = ' ';
-char notvisited = 'X';
+char empty = -1;
+char notvisited = 0;
 
-int getgrid(std::string filename, std::string **grid){
+int getgrid(std::string filename, int **grid){
 	std::ifstream infile(filename);
-	std::string placeholder[20];
 	int i=0;
-	while(std::getline(infile,placeholder[i]))
+	std::string placeholder;
+	while(std::getline(infile, placeholder)){
 		i++;
+		for (int j = 0; j < placeholder.size(); j++){
+			if (placeholder[j]==' ')
+				grid[i-1][j]=-1;
+			else
+				grid[i-1][j]=0;
+		}
+	}
 	return i;
 }
-// void printgrid(std::string grid[][20],int lines){
-// 	std::cout << "[0;0H\n";
-// 	for (int i = 0; i < lines; i++){
-// 		for (int j = 0; j < lines; j++)
-// 			std::cout<<grid[i][j];
-// 		std::cout<<"\n";
-// 	}
-// }
-// void plant(std::string *grid, int row, int col){
-// 	if (grid[row][col]==notvisited)
-// 		grid[row][col]=knight;
-// 	else
-// 		std::cout<<"You can not plant a knight in that location\n";
-// }
-// void solve (std::string *grid, int row, int col, bool &done, int num, int lines){
-// 	// plant(grid,row,col);
-// 	if (grid[row][col]==notvisited){
-// 		grid[row][col] = "" + (num+1);
-// 		if (!done)
-// 			solve(grid, row-2, col-1,done,num+1, lines);
-// 		if (!done)
-// 			solve(grid, row-2, col+1,done,num+1, lines);
-// 		if (!done)
-// 			solve(grid, row-1, col-2,done,num+1, lines);
-// 		if (!done)
-// 			solve(grid, row-1, col+2,done,num+1, lines);
-// 		if (!done)
-// 			solve(grid, row+2, col+1,done,num+1, lines);
-// 		if (!done)
-// 			solve(grid, row+2, col-1,done,num+1, lines);
-// 		if (!done)
-// 			solve(grid, row+1, col-2,done,num+1, lines);
-// 		if (!done)
-// 			solve(grid, row+1, col+2,done,num+1, lines);
-// 	}
-// 	else{
-// 		return;
-// 	}
-// 	int count = 0;
-// 	for (int i = 0; i < lines; i++){
-// 		for (int j = 0; j < lines;j++){
-// 			if (grid[i][j]==notvisited)
-// 				count++;
-// 		}
-// 	}
-// 	if (count==0)
-// 		done=true;
-// 	if (done)
-// 		grid[row][col]=num;
-// 	else{
-// 		for (int i = 0; i < lines; i++){
-// 			for (int j = 0; j < lines;j++){
-// 				if (grid[i][j]>num)
-// 					grid[i][j]=notvisited;
-// 			}
-// 		}
-// 	}
-// 	usleep(80000);
-// 	printgrid(grid, lines);
-// }
+void printgrid(int **grid,int lines){
+	std::cout << "[0;0H\n";
+	for (int i = 0; i < lines; i++){
+		for (int j = 0; j < lines; j++){
+			if (grid[i][j]==-1)
+				std::cout<<std::setw(3)<<".";
+			else
+				std::cout<<std::setw(3)<<grid[i][j];
+		}
+		std::cout<<"\n";
+	}
+}
+void plant(int **grid, int row, int col){
+	if (grid[row][col]==notvisited)
+		grid[row][col]=1;
+	else
+		std::cout<<"You can not plant a knight in that location\n";
+}
+void solve (int **grid, int row, int col, bool &done, int num, int lines){
+	// plant(grid,row,col);
+	usleep(800);
+	printgrid(grid, lines);
+	if (grid[row][col]==notvisited){
+		grid[row][col] = num+1;
+		if (!done)
+			solve(grid, row-2, col-1,done,num+1, lines);
+		if (!done)
+			solve(grid, row-2, col+1,done,num+1, lines);
+		if (!done)
+			solve(grid, row-1, col-2,done,num+1, lines);
+		if (!done)
+			solve(grid, row-1, col+2,done,num+1, lines);
+		if (!done)
+			solve(grid, row+2, col+1,done,num+1, lines);
+		if (!done)
+			solve(grid, row+2, col-1,done,num+1, lines);
+		if (!done)
+			solve(grid, row+1, col-2,done,num+1, lines);
+		if (!done)
+			solve(grid, row+1, col+2,done,num+1, lines);
+	}
+	else{
+		return;
+	}
+	int count = 0;
+	for (int i = 0; i < lines; i++){
+		for (int j = 0; j < lines;j++){
+			if (grid[i][j]==notvisited)
+				count++;
+		}
+	}
+	if (count==0)
+		done=true;
+	else{
+		for (int i = 0; i < lines; i++){
+			for (int j = 0; j < lines;j++){
+				if (grid[i][j]>num)
+					grid[i][j]=notvisited;
+			}
+		}
+	}
+}
 int main(){
 	bool done = false;
-	std::string grid[20][20];
+	int** grid = new int*[20];
+	for (int i = 0; i < 20; i++) {
+        grid[i] = new int[20];
+    }
 	int lines = getgrid("grid.dat",grid);
-	// printgrid(grid,lines);
-	// solve(grid,3,3, done, 0, lines);
+	printgrid(grid,lines);
+	// plant(grid,3,3);
+	printgrid(grid,lines);
+	solve(grid,3,3, done, 0, lines);
 	// printgrid(grid,lines);
 	return 0;
 }
