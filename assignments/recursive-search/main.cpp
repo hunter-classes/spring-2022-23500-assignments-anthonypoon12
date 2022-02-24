fea1e37e4f426ee9a38f10606a68b7a3a758150d
anthonypoon12 <anthony@poons.us>
2022-02-23 23:59:34 -0500

    fixed up main to look better and added second case

--- assignments/recursive-search/main.cpp
+++ assignments/recursive-search/main.cpp
@@ -7,7 +7,16 @@ int main(){
         grid[i] = new int[20];
     }
 	int lines = getgrid("grid.dat",grid);
-	solve(grid,6,2, done, 0, lines);
+	std::cout<<"upper left corner is (0,0),\nbottom right corner is (4,0),\ntop right corner is (0,4),\n bottom right corner is (4,4)\nknight starting point is (0,0)\n";
+	solve(grid,2,2, done, 0, lines);
+	printgrid(grid,lines);
+	for (int i = 0; i < 20; i++)
+		for (int j = 0; j<20; j++)
+			if (grid[i][j]!=-1)
+				grid[i][j]=0;
+	done=false;
+	std::cout<<"knight starting at (3,3)\n";
+	solve(grid,4,4, done, 0, lines);
 	printgrid(grid,lines);
 	return 0;
 }
