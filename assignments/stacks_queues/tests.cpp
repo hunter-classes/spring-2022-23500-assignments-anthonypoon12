c907f45263d6774120c2e88c910e108da4cee333
anthonypoon12 <anthony@poons.us>
2022-04-27 16:56:44 -0400

    tested push

--- assignments/stacks_queues/tests.cpp
+++ assignments/stacks_queues/tests.cpp
@@ -4,9 +4,18 @@
 #include "stack.h"
 
 stack *stack1 = new stack();
-Node *node1 = new Node(1);
 TEST_CASE("Creating a stack"){
     CHECK(stack1->is_empty()==true);
     CHECK_THROWS(stack1->pop());
     CHECK_THROWS(stack1->top());
+}
+TEST_CASE("Testing Push"){
+    stack1->push(0);
+    CHECK(!stack1->is_empty());
+    stack1->push(1);
+    CHECK(!stack1->is_empty());
+    CHECK(stack1->top()==1);
+    CHECK(stack1->pop()==1);
+    CHECK(stack1->pop()==0);
+    CHECK(stack1->is_empty());
 }
