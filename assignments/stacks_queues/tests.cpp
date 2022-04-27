18e8a993f2827faeed5ac89f3f9f34ccbf3ce29d
anthonypoon12 <anthony@poons.us>
2022-04-27 12:31:45 -0400

    Added first tests

--- assignments/stacks_queues/tests.cpp
+++ assignments/stacks_queues/tests.cpp
@@ -3,3 +3,10 @@
 #include "doctest.h"
 #include "stack.h"
 
+stack *stack1 = new stack();
+Node *node1 = new Node(1);
+TEST_CASE("Creating a stack"){
+    CHECK(stack1->is_empty()==true);
+    CHECK_THROWS(stack1->pop());
+    CHECK_THROWS(stack1->top());
+}
