7751555684322e73146f83f9919cd61b182e0317
anthonypoon12 <anthony@poons.us>
2022-04-27 16:59:33 -0400

    Edited comments on tests and deleted stack

--- assignments/stacks_queues/tests.cpp
+++ assignments/stacks_queues/tests.cpp
@@ -4,12 +4,12 @@
 #include "stack.h"
 
 stack *stack1 = new stack();
-TEST_CASE("Creating a stack"){
+TEST_CASE("Creating a stack, checking errors for pop and top, checking is empty"){
     CHECK(stack1->is_empty()==true);
     CHECK_THROWS(stack1->pop());
     CHECK_THROWS(stack1->top());
 }
-TEST_CASE("Testing Push"){
+TEST_CASE("Testing Push, Push, Top"){
     stack1->push(0);
     CHECK(!stack1->is_empty());
     stack1->push(1);
@@ -18,4 +18,5 @@ TEST_CASE("Testing Push"){
     CHECK(stack1->pop()==1);
     CHECK(stack1->pop()==0);
     CHECK(stack1->is_empty());
+    delete stack1;
 }
