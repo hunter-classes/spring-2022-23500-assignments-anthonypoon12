0c39e3d6d9c0885dcb55eed4900cb892a1caec09
anthonypoon12 <anthony@poons.us>
2022-05-13 17:50:07 -0400

    Implemented all functions in main

--- assignments/trees/main.cpp
+++ assignments/trees/main.cpp
@@ -4,15 +4,65 @@
 int main(){
     std::cout<<"Creating tree....\n";
     Tree *tree = new Tree();
+    std::cout<<std::boolalpha;
+    std::cout<<"Visual Representation of Tree:\n\n\n\n";
+    tree->pretty_printer();
+    std::cout<<"Height of tree is: "<<tree->height()<<"\n";
+    std::cout<<"Number of leaves is: "<<tree->count_leaves()<<"\n";
+    std::cout<<"\n\n\n";
     std::cout<<"Searching for 10: ";
     try{
         std::cout<<tree->rsearch(10)<<"\n";
     }catch(int e){
         std::cout<<"Not found\n";
     }
-    std::cout<<"Adding 1,2,3,8,42,56,7,8,9063426, -10, -30, -25, -2,-1 to tree\n";
-    for (auto x: {1,2,3,8,42,56,7,8,9063426, -10, -30, -25, -2,-1 })
+    std::cout<<"Adding 1,2,3,8,42,56,7, 9063426, -10, -30, -25, -2,-1, 50, 51 to tree\n";
+    for (auto x: {1,2,3,8,42,56,7,9063426, -10, -30, -25, -2,-1, 50, 51})
         tree->insert(x);
+    std::cout<<"Visual Representation of Tree:\n\n\n\n";
+    tree->pretty_printer();
+    std::cout<<"Height of tree is: "<<tree->height()<<"\n";
+    std::cout<<"Number of leaves is: "<<tree->count_leaves()<<"\n";
+    std::cout<<"Sum of nodes at level 3: ";
+    try{
+        std::cout<<tree->sum_at_level(3)<<"\n";
+    }catch(int e){
+        std::cout<<"Tree height is not large enough\n";
+    }
+    std::cout<<"Sum of nodes at level 10: ";
+    try{
+        std::cout<<tree->sum_at_level(10)<<"\n";
+    }catch(int e){
+        std::cout<<"Tree height is not large enough\n";
+    }
+    std::cout<<"Sum of nodes at level 2: ";
+    try{
+        std::cout<<tree->sum_at_level(2)<<"\n";
+    }catch(int e){
+        std::cout<<"Tree height is not large enough\n";
+    }
+    std::cout<<"-30 and 3 are cousins: ";
+    try{
+        std::cout<< tree->cousins(-30,3)<<"\n";
+    }
+    catch(int e){
+        std::cout<<"Not all numbers are not in tree\n";
+    }
+    std::cout<<"-30 and 1000 are cousins: ";
+    try{
+        std::cout<< tree->cousins(-30,1000)<<"\n";
+    }
+    catch(int e){
+        std::cout<<"Not all numbers are not in tree\n";
+    }
+    std::cout<<"-30 and -25 are cousins: ";
+    try{
+        std::cout<< tree->cousins(-30,-25)<<"\n";
+    }
+    catch(int e){
+        std::cout<<"Not all numbers are not in tree\n";
+    }
+    std::cout<<"\n\n\n";
     for (auto x: {1,2,3,8,42,30,56,7,8,9063426,2000, -10, -30, -25, -2,-1}){
         std::cout<<"Searching for "<<x<<": ";
         try{
@@ -21,42 +71,19 @@ int main(){
             std::cout<<"Not found\n";
         }
     }
+    for (auto x: {2,1,4,6,8}){
+        std::cout<<"Removing number "<<x<<": ";
+        try{
+            tree->remove(x);
+            std::cout<<"Sucess!\n";
+        }catch(int e){
+            std::cout<<"Failed\n";
+        }
+    }
+    std::cout<<"Visual Representation of Tree:\n\n\n\n";
     tree->pretty_printer();
-    std::cout<<"Removing number 2: ";
-    try{
-        tree->remove(2);
-        std::cout<<"Sucess!\n";
-    }catch(int e){
-        std::cout<<"Failed\n";
-    }
-    std::cout<<"Removing number 1: ";
-    try{
-        tree->remove(1);
-        std::cout<<"Sucess!\n";
-    }catch(int e){
-        std::cout<<"Failed\n";
-    }
-    std::cout<<"Removing number 4: ";
-    try{
-        tree->remove(4);
-        std::cout<<"Sucess!\n";
-    }catch(int e){
-        std::cout<<"Failed\n";
-    }
-    std::cout<<"Removing number 6: ";
-    try{
-        tree->remove(6);
-        std::cout<<"Sucess!\n";
-    }catch(int e){
-        std::cout<<"Failed\n";
-    }
-    std::cout<<"Removing number 8: ";
-    try{
-        tree->remove(8);
-        std::cout<<"Sucess!\n";
-    }catch(int e){
-        std::cout<<"Failed\n";
-    }
-    tree->pretty_printer();
+    std::cout<<"Height of tree is: "<<tree->height()<<"\n";
+    std::cout<<"Number of leaves is: "<<tree->count_leaves()<<"\n";
+    std::cout<<"\n\n\n";
     return 0;
 }
