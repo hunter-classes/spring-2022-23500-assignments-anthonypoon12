ba625bd659560ad06a731f47349b73757c13daf4
anthonypoon12 <anthony@poons.us>
2022-05-13 19:13:06 -0400

    Made constructor

--- assignments/hashing/Dictionary.cpp
+++ assignments/hashing/Dictionary.cpp
@@ -1,1 +1,19 @@
 #include "Dictionary.h"
+
+Dictionary::Dictionary(){
+    for (int i = 0; i < 10; i++){
+        list[i]= new List();
+    }
+}
+Dictionary::~Dictionary(){
+
+}
+void Dictionary::insert(Person *person){
+    
+}
+Person* Dictionary::retrieve(int id){
+    return nullptr;
+}
+std::string Dictionary::allKeys(){
+    return "";
+}
