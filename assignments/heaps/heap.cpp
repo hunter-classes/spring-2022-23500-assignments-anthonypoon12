2bbd318fb09325fbe55cbdc8dba9728d6bdb9164
anthonypoon12 <anthony@poons.us>
2022-05-14 19:56:52 -0400

    Made function to turn array into heap (untested)

--- assignments/heaps/heap.cpp
+++ assignments/heaps/heap.cpp
@@ -1,3 +1,28 @@
 #include "heap.h"
 void heapsort(int data[]){
+    int size = sizeof(data)/sizeof(data[0]);
+    if (size>0){
+        heapify(data, size);
+    }
+}
+void heapify(int data[], int size){
+    heapify(data, 0, size);
+}
+void heapify(int data[], int index, int size){
+    if (index*2+1<size){
+        heapify(data,index * 2 + 1);
+        if (index*2 + 2<size){
+            heapify(data, index *2 + 2);
+            if (data[index*2+2]<data[index*2+1])
+                if (data[index*2+2]<data[index])
+                    swap(data, index,index*2+2);
+        }
+        if (data[index*2+1]<data[index])
+            swap(data, index, index*2+1);
+    }
+}
+void swap(int data[], int indone, int indtwo){
+    int dummy = data[indone];
+    data[indone] = data[indtwo];
+    data[indtwo]=dummy;
 }
--- assignments/heaps/heap.h
+++ assignments/heaps/heap.h
@@ -1,2 +1,4 @@
 #pragma once
 void heapsort(int data[]);
+void heapify(int data[], int size);
+void heapify(int data[], int index, int size);
