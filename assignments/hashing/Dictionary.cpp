283d978a0b8deda28a1fef5da624411bd94bb607
anthonypoon12 <anthony@poons.us>
2022-05-13 22:07:47 -0400

    added deconstructer, insert, retrieve, and allkeys

--- assignments/hashing/Dictionary.cpp
+++ assignments/hashing/Dictionary.cpp
@@ -1,19 +1,30 @@
 #include "Dictionary.h"
-
 Dictionary::Dictionary(){
     for (int i = 0; i < 10; i++){
         list[i]= new List();
     }
 }
 Dictionary::~Dictionary(){
-
+    for (auto x: list)
+        delete x;
 }
 void Dictionary::insert(Person *person){
-    
+    list[hashfunc(person->get_name(), 10)]->insert(person);
 }
-Person* Dictionary::retrieve(int id){
-    return nullptr;
+Person* Dictionary::retrieve(std::string name){
+    return list[hashfunc(name, 10)]->findByName(name);
 }
 std::string Dictionary::allKeys(){
-    return "";
+    std::string output = "";
+    for (auto x: list){
+        output+=x->toString();
+    }
+    return output.substr(0,output.size()-2);
+}
+int Dictionary::hashfunc(std::string x, int M) {
+    int xlength = x.length();
+    int i, sum;
+    for (sum=0, i=0; i < x.length(); i++)
+    sum += x[i];
+    return sum % M;
 }
