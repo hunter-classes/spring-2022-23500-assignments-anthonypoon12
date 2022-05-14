db97f13d1ef6f0c974af72b6e5cceb29202ee45e
anthonypoon12 <anthony@poons.us>
2022-05-13 22:07:53 -0400

    added tests and main

--- assignments/hashing/main.cpp
+++ assignments/hashing/main.cpp
@@ -1,5 +1,45 @@
 #include <iostream>
 #include "Dictionary.h"
 int main(){
+    std::cout<<"Creating Dictionary...\n";
+    Dictionary *dictionary = new Dictionary();
+    std::cout<<"Retrieving a random name from dictionary: "<<dictionary->retrieve("name")<<"\n";
+    std::cout<<"All Keys right now: "<<dictionary->allKeys()<<"\n";
+    std::cout<<"Inserting Anthony Poon\n";
+    dictionary->insert(new Person("Anthony", "Poon", 1));
+    std::cout<<"Inserting Hannibal Lecter\n";
+    dictionary->insert(new Person("Hannibal", "Lecter", 2));
+    std::cout<<"Inserting Hunter Mascot\n";
+    dictionary->insert(new Person("Hunter", "Mascot", 3));
+    std::cout<<"Inserting John Boyardee\n";
+    dictionary->insert(new Person("John", "Boyardee", 4));
+    std::cout<<"Inserting Pikachu Charizard\n";
+    dictionary->insert(new Person("Pikachu", "Charizard", 5));
+    std::cout<<"Inserting Ash Ketchum\n";
+    dictionary->insert(new Person("Ash", "Ketchum", 6));
+    std::cout<<"Inserting Kim Hi\n";
+    dictionary->insert(new Person("Kim", "Hi", 7));
+    std::cout<<"Inserting Mike Make\n";
+    dictionary->insert(new Person("Mike", "Make", 8));
+    std::cout<<"Inserting Creative Name\n";
+    dictionary->insert(new Person("Creative", "Name", 9));
+    std::cout<<"Inserting Last One\n";
+    dictionary->insert(new Person("Last", "One", 10));
+    std::cout<<"The name assosciated with Poon, Anthony is: ";
+    if(dictionary->retrieve("Poon, Anthony")!=nullptr)
+        std::cout<<dictionary->retrieve("Poon, Anthony")->get_name()<<"\n";
+    else
+        std::cout<<"Not in dictionary\n";
+    std::cout<<"The name assosciated with Make, Mike is: ";
+    if(dictionary->retrieve("Make, Mike")!=nullptr)
+        std::cout<<dictionary->retrieve("Make, Mike")->get_name()<<"\n";
+    else
+        std::cout<<"Not in dictionary\n";
+    std::cout<<"The name assosciated with NotAReal, Name is: ";
+    if(dictionary->retrieve("NotAReal, Name")!=nullptr)
+        std::cout<<dictionary->retrieve("NotAReal, Name")->get_name()<<"\n";
+    else
+        std::cout<<"Not in dictionary\n";
+    delete dictionary;
     return 0;
 }
--- assignments/hashing/tests.cpp
+++ assignments/hashing/tests.cpp
@@ -2,6 +2,27 @@
 #include "doctest.h"
 #include "Dictionary.h"
 
-TEST_CASE("Template"){
-    CHECK(true);
+Dictionary *dictionary = new Dictionary();
+Person *Anthony = new Person("Anthony", "Poon", 1);
+Person *Hunter = new Person("Hunter", "College", 2);
+Person *Baruch = new Person("Baruch", "School", 2);
+Person *idkman = new Person("IDK", "Man", 2);
+Person *whatdoiput = new Person("WhatDo", "IPut", 2);
+Person *arandomname = new Person("ARandom", "Name", 2);
+TEST_CASE("insert and allKeys"){
+    dictionary->insert(Anthony);
+    dictionary->insert(Hunter);
+    dictionary->insert(Baruch);
+    dictionary->insert(idkman);
+    dictionary->insert(whatdoiput);
+    dictionary->insert(arandomname);
+}
+TEST_CASE("Retrieve"){
+    CHECK(dictionary->retrieve("Poon, Anthony")==Anthony);
+    CHECK(dictionary->retrieve("College, Hunter")==Hunter);
+    CHECK(dictionary->retrieve("School, Baruch")==Baruch);
+    CHECK(dictionary->retrieve("Man, IDK")==idkman);
+    CHECK(dictionary->retrieve("IPut, WhatDo")==whatdoiput);
+    CHECK(dictionary->retrieve("Name, ARandom")==arandomname);
+    delete dictionary;
 }
