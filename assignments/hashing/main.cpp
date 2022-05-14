1863a5f1434c913b5304d14d23039584b9855b9f
anthonypoon12 <anthony@poons.us>
2022-05-14 02:09:33 -0400

    Tested hashing and addresses in main and tests

--- assignments/hashing/main.cpp
+++ assignments/hashing/main.cpp
@@ -8,38 +8,54 @@ int main(){
         std::cout<<dictionary->retrieve("random, name")->get_name()<<"\n";
     else
         std::cout<<"Not in dictionary\n";
-    std::cout<<"All Keys right now: "<<dictionary->allKeys()<<"\n\n\n";
+    Person *Anthony = new Person("Anthony", "Poon", 1);
+    Person *Hannibal =new Person("Hannibal", "Lecter", 2);
+    Person *Hunter =new Person("Hunter", "Mascot", 3);
+    Person *John =new Person("John", "Boyardee", 4);
+    Person *Pikachu =new Person("Pikachu", "Charizard", 5);
+    Person *Ash =new Person("Ash", "Ketchum", 6);
+    Person *Kim =new Person("Kim", "Hi", 7);
+    Person *Mike = new Person("Mike", "Make", 8);
+    Person *Creative =new Person("Creative", "Name", 9);
+    Person *Last =new Person("Last", "One", 10);
+    std::cout<<"All Keys right now:\n"<<dictionary->allKeys()<<"\n\n\n";
     std::cout<<"Inserting Anthony Poon\n";
-    dictionary->insert(new Person("Anthony", "Poon", 1));
+    dictionary->insert(Anthony);
     std::cout<<"Inserting Hannibal Lecter\n";
-    dictionary->insert(new Person("Hannibal", "Lecter", 2));
+    dictionary->insert(Hannibal);
     std::cout<<"Inserting Hunter Mascot\n";
-    dictionary->insert(new Person("Hunter", "Mascot", 3));
+    dictionary->insert(Hunter);
     std::cout<<"Inserting John Boyardee\n";
-    dictionary->insert(new Person("John", "Boyardee", 4));
+    dictionary->insert(John);
     std::cout<<"Inserting Pikachu Charizard\n";
-    dictionary->insert(new Person("Pikachu", "Charizard", 5));
+    dictionary->insert(Pikachu);
     std::cout<<"Inserting Ash Ketchum\n";
-    dictionary->insert(new Person("Ash", "Ketchum", 6));
+    dictionary->insert(Ash);
     std::cout<<"Inserting Kim Hi\n";
-    dictionary->insert(new Person("Kim", "Hi", 7));
+    dictionary->insert(Kim);
     std::cout<<"Inserting Mike Make\n";
-    dictionary->insert(new Person("Mike", "Make", 8));
+    dictionary->insert(Mike);
     std::cout<<"Inserting Creative Name\n";
-    dictionary->insert(new Person("Creative", "Name", 9));
+    dictionary->insert(Creative);
     std::cout<<"Inserting Last One\n";
-    dictionary->insert(new Person("Last", "One", 10));
-    std::cout<<"All Keys right now: "<<dictionary->allKeys()<<"\n\n\n";
+    dictionary->insert(Last);
+    for (auto x: {Anthony, Hannibal, Hunter, John, Pikachu, Ash, Kim, Mike, Creative, Last}){
+        std::cout<<"Hash Function value of "<< x->get_name()<<" is "<< dictionary->hashfunc(x->get_name(), 10)<<'\n';
+    }
+    std::cout<<"All Keys right now:\n"<<dictionary->allKeys()<<"\n\n\n";
+    std::cout<<"The address assosciated with Poon, Anthony is: "<<dictionary->retrieve("Poon, Anthony")<<"\n";
     std::cout<<"The name assosciated with Poon, Anthony is: ";
     if(dictionary->retrieve("Poon, Anthony")!=nullptr)
         std::cout<<dictionary->retrieve("Poon, Anthony")->get_name()<<"\n";
     else
         std::cout<<"Not in dictionary\n";
+    std::cout<<"The Address assosciated with Make, Mike is: "<<dictionary->retrieve("Make, Mike")<<"\n";
     std::cout<<"The name assosciated with Make, Mike is: ";
     if(dictionary->retrieve("Make, Mike")!=nullptr)
         std::cout<<dictionary->retrieve("Make, Mike")->get_name()<<"\n";
     else
         std::cout<<"Not in dictionary\n";
+    std::cout<<"The Address assosciated with NotAReal, Name is: "<<dictionary->retrieve("NotAReal, Name")<<"\n";
     std::cout<<"The name assosciated with NotAReal, Name is: ";
     if(dictionary->retrieve("NotAReal, Name")!=nullptr)
         std::cout<<dictionary->retrieve("NotAReal, Name")->get_name()<<"\n";
--- assignments/hashing/tests.cpp
+++ assignments/hashing/tests.cpp
@@ -8,7 +8,15 @@ Person *Hunter = new Person("Hunter", "College", 2);
 Person *Baruch = new Person("Baruch", "School", 2);
 Person *idkman = new Person("IDK", "Man", 2);
 Person *whatdoiput = new Person("WhatDo", "IPut", 2);
-Person *arandomname = new Person("ARandom", "Name", 2);
+Person *arandomname = new Person("ARandomfawdafeg", "Name", 2);
+TEST_CASE("Hash function"){
+    CHECK(dictionary->hashfunc(Anthony->get_name(),10)==5);
+    CHECK(dictionary->hashfunc(Hunter->get_name(),10)==5);
+    CHECK(dictionary->hashfunc(Baruch->get_name(),10)==9);
+    CHECK(dictionary->hashfunc(idkman->get_name(),10)==6);
+    CHECK(dictionary->hashfunc(whatdoiput->get_name(),10)==5);
+    CHECK(dictionary->hashfunc(arandomname->get_name(),10)==6);
+}
 TEST_CASE("insert and allKeys"){
     dictionary->insert(Anthony);
     dictionary->insert(Hunter);
@@ -23,6 +31,6 @@ TEST_CASE("Retrieve"){
     CHECK(dictionary->retrieve("School, Baruch")==Baruch);
     CHECK(dictionary->retrieve("Man, IDK")==idkman);
     CHECK(dictionary->retrieve("IPut, WhatDo")==whatdoiput);
-    CHECK(dictionary->retrieve("Name, ARandom")==arandomname);
+    CHECK(dictionary->retrieve("Name, ARandomfawdafeg")==arandomname);
     delete dictionary;
 }
