9e1fa0ccbc2593ac76fcec80391fff32b3b3a743
anthonypoon12 <anthony@poons.us>
2022-05-14 22:59:29 -0400

    Fixed heapify and added sift down

--- assignments/heaps/heap.cpp
+++ assignments/heaps/heap.cpp
@@ -1,6 +1,7 @@
 #include "heap.h"
+#include <iostream>
 void heapsort(int data[]){
-    int size = sizeof(data)/sizeof(data[0]);
+    int size = sizeof(&data)/sizeof(&data[0]);
     if (size>0){
         heapify(data, size);
     }
@@ -10,19 +11,32 @@ void heapify(int data[], int size){
 }
 void heapify(int data[], int index, int size){
     if (index*2+1<size){
-        heapify(data,index * 2 + 1);
+        heapify(data,index * 2 + 1, size);
         if (index*2 + 2<size){
-            heapify(data, index *2 + 2);
-            if (data[index*2+2]<data[index*2+1])
-                if (data[index*2+2]<data[index])
-                    swap(data, index,index*2+2);
+            heapify(data, index * 2 + 2, size);
         }
-        if (data[index*2+1]<data[index])
-            swap(data, index, index*2+1);
     }
+    sift_down(data, index, size);
 }
 void swap(int data[], int indone, int indtwo){
     int dummy = data[indone];
     data[indone] = data[indtwo];
     data[indtwo]=dummy;
+}
+void sift_down(int data[], int index, int size){
+    if (index*2+1<size){
+        if (index*2+2<size){
+            if (data[index*2+2]<data[index*2+1]){
+                if (data[index*2+2]<data[index]){
+                    swap(data,index,index*2+2);
+                    sift_down(data, index*2+2,size);
+                    return;
+                }
+            }
+        }
+        if (data[index*2+1]<data[index]){
+            swap(data,index,index*2+1);
+            sift_down(data, index*2+1,size);
+        }
+    }
 }
--- assignments/heaps/heap.h
+++ assignments/heaps/heap.h
@@ -2,3 +2,5 @@
 void heapsort(int data[]);
 void heapify(int data[], int size);
 void heapify(int data[], int index, int size);
+void swap(int data[], int indone, int indtwo);
+void sift_down(int data[], int index, int size);
