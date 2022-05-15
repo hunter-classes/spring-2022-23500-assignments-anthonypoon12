753a825a2cde82c9f9fb5f6fd0509e4501fb453f
anthonypoon12 <anthony@poons.us>
2022-05-14 22:59:45 -0400

    added heapify to main

--- assignments/heaps/main.cpp
+++ assignments/heaps/main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
 #include "heap.h"
 int main(){
+    int arr[] = {4,7,9,1,0,2,-12,245,3,74,236};
+    std::cout<<"Initial Array 1: ";
+    for (auto x: arr)
+        std::cout<<x<<", ";
+    std::cout<<"\n";
+    int arr2[] = {900,1,-4,-20,-100,-75,0,-5,25,7,-3,-13,250};
+    heapify(arr, sizeof(arr)/sizeof(arr[0]));
+    std::cout<<"Heapified Array 1: ";
+    for (auto x: arr)
+        std::cout<<x<<", ";
+    std::cout<<"\n";
+    std::cout<<"Initial Array 2: ";
+    for (auto x: arr2)
+        std::cout<<x<<", ";
+    std::cout<<"\n";
+    heapify(arr2, sizeof(arr2)/sizeof(arr2[0]));
+    std::cout<<"Heapified Array 2: ";
+    for (auto x: arr2)
+        std::cout<<x<<", ";
+    std::cout<<"\n";
     return 0;
 }
